b0591f72ad77c7df9969c46eabe04ebc1aaeb034
lc37819 <lc37819@umbc.edu>
2024-06-21 15:25:05 -0400

    Adding retrieval of user command

--- simple_shell.c
+++ simple_shell.c
@@ -14,6 +14,7 @@ description: a simple linux shell designed to perform basic linux commands
 
 // DEFINE THE FUNCTION PROTOTYPES
 void user_prompt_loop(void);
+char *get_user_command(void);
 
 int main(int argc, char **argv) {
 
@@ -29,11 +30,11 @@ int main(int argc, char **argv) {
 
 /*Prompt the user for a new command */
 void user_prompt_loop(void) {
-    char command[100];
-
+    char *command;
+    
     while(1) {
         printf(">> ");
-        fgets(command, 100, stdin);
+        command = get_user_command();
         size_t length = strlen(command);
 
         if (length > 0 && command[length - 1] == '\n') {
@@ -41,7 +42,45 @@ void user_prompt_loop(void) {
         }
 
         if (strcmp(command, "exit") == 0) {
+            free(command);
             exit(0);
         }
+
+        free(command);
+    }
+}
+
+
+/* Take input of arbitrary length from the user and returns it to user_prompt_loop() */
+char *get_user_command(void) {
+    char *command = NULL;
+    int buffer_size = 100;
+    int length = 0;
+    char temp;
+
+    command = (char *)malloc(buffer_size * sizeof(char));
+    if (!command) {
+        fprintf(stderr, "Error: Memory allocation failed.\n");
+        exit(1);
+    }
+
+    while(1) {
+        temp = getchar();
+
+        if (length >= buffer_size -1) {
+            buffer_size *= 2;
+            command = realloc(command, buffer_size);
+            if (!command) {
+                fprintf(stderr, "Error: Memory reallocation failed.\n");
+                exit(1);
+            }
+        }
+
+        if (temp == EOF || temp == '\n') {
+            command[length] = '\0';
+            return command;
+        }
+
+        command[length++] = temp;
     }
 }
